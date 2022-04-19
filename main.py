83a113eaec3f9030e2dde7352f478a89d65fcecd
Faisal Ali <fzl.ali33@gmail.com>
2022-04-19 22:52:50 +0530

    Update main.py
    Dynamically created the cells on grid

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 # Import libraries
 from tkinter import *
+from cell import Cell
 import settings
 import utils
 
@@ -22,6 +23,13 @@ left_frame.place(x = 0, y = utils.height_prct(25))
 # Adding a center frame
 center_frame = Frame(root, width=utils.width_prct(75), height=utils.height_prct(75))
 center_frame.place(x = utils.width_prct(25), y = utils.height_prct(25))
+
+# Creating buttons on a grid
+for x in range(settings.GRID_SIZE):
+    for y in range(settings.GRID_SIZE):
+        c = Cell(x, y)
+        c.create_btn_object(center_frame)
+        c.cell_btn_object.grid(column = x, row = y)
 
 # Keep the window running
 root.mainloop()
