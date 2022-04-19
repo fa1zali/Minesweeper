d2fb8b8b837f39f69077fae96cb0dae60a49c930
Faisal Ali <fzl.ali33@gmail.com>
2022-04-19 22:06:05 +0530

    Update main.py
    Added all frames

--- main.py
+++ main.py
@@ -12,12 +12,16 @@ root.title("Minesweeper")
 root.resizable(False, False)
 
 # Adding a frame at the top
-top_frame = Frame(root, bg = "teal", width = settings.WIDTH, height = utils.height_prct(25))
+top_frame = Frame(root, width = settings.WIDTH, height = utils.height_prct(25))
 top_frame.place(x = 0, y = 0)
 
 # Adding a frame to the left
-left_frame = Frame(root, bg = "teal", width = utils.width_prct(25), height = utils.height_prct(75))
+left_frame = Frame(root, width = utils.width_prct(25), height = utils.height_prct(75))
 left_frame.place(x = 0, y = utils.height_prct(25))
+
+# Adding a center frame
+center_frame = Frame(root, width=utils.width_prct(75), height=utils.height_prct(75))
+center_frame.place(x = utils.width_prct(25), y = utils.height_prct(25))
 
 # Keep the window running
 root.mainloop()
